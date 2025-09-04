5e642123c7a853588d912ef7815e400d02e401d8
¨raystef66¨ <s.vanbarel@gmail.com>
2025-09-04 08:18:47 +0200

    cepheus: Support forcing ext4 when formatting data
    Prevents bootloop on Android 16 due to mismatched fscrypt policies

--- device.mk
+++ device.mk
@@ -30,6 +30,8 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_RECOVERY_DEVICE_MODULES += libion libxml2 libicuuc vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
+OF_WIPE_METADATA_AFTER_DATAFORMAT := 1
+OF_FIX_DECRYPTION_ON_DATA_MEDIA := 1
 
 PRODUCT_SOONG_NAMESPACES += \
 vendor/qcom/opensource/commonsys-intf/display
